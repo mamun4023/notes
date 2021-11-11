a6f2522021ed55b89a3022725db8d64c83759630
Mamunur Rashid <40039219+mamun4023@users.noreply.github.com>
2021-11-11 10:04:57 +0600

    Delete Web development/front end/React/react testing/1 test directory

--- Web development/front end/React/react testing/1 test/home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './home';
-
-test('renders learn react link', () => {
-  render(<Home name = "akbar"/>);
-  const linkElement = screen.getByText(/akbar/i);
-  expect(linkElement).toBeInTheDocument();
-});
